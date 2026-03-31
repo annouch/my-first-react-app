e3e6d6b42c987d6724c982816d585ef2a882aa98
Yassine ANNOUCH <annouch.yassine@gmail.com>
2026-03-31 13:09:43 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from './logo.svg';
+import './App.css';
+import { Component } from 'react';
+import Hello from "./components/Hello";
+function App() {
+
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+{/*         <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> 
+        <Hello />*/}
+
+        <Hello nom="Yassine" />
+      </header>
+    </div>
+  );
+}
+
+/* class Hello extends Component{
+
+
+  render(){
+    return <h1>Hello world.</h1>
+  }
+} */
+
+
+/* function Bonjour() {
+  return <h1>Bonjour 👋</h1>;
+} */
+
+export default App;
---
+++ src/components/Hello.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class Helloo extends Component {
+  render() {
+    return <h1>HELLOO WORLD {this.props.nom}</h1>;
+  }
+}
+
+export default Helloo;
